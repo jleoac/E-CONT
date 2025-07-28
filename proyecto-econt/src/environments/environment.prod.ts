1d52e61d9bde01a53daac0d4450da29d85131d1d
Leonardo Astudillo <jleoac@hotmail.com>
2025-07-28 12:58:55 -0500

    ajuste en Global.url

--- proyecto-econt/src/environments/environment.prod.ts
+++ proyecto-econt/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyecto-web-econt-production.up.railway.app/api/'
+  apiUrl: 'https://ekont-production.up.railway.app/api/'
 };
